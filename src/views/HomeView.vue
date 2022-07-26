768f3b630216a93bff2efd3bb24df3324af57843
EstherChen000 <estherchen000@gmail.com>
2022-07-26 17:15:11 +0800

    Unit Testing Vue 3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,23 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <RandomNumber />
+    <MessageDisplay />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import RandomNumber from "@/components/RandomNumber.vue";
+import MessageDisplay from "@/components/MessageDisplay.vue";
 
 export default {
   name: "HomeView",
   components: {
     HelloWorld,
+    RandomNumber,
+    MessageDisplay,
   },
 };
 </script>
